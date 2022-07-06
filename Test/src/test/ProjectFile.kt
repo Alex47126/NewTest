dbac1037db513438ed762250fde809bd4d0628e3
Alex47126 <107268223+Alex47126@users.noreply.github.com>
2022-07-06 22:51:16 +0300

    Add files via upload

---
+++ Test/src/test/ProjectFile.kt
@@ -0,0 +1,10 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class ProjectFile {
+    @Test
+    fun test() {
+        assertThat(true).isEqualTo(true)
+        assertThat(false).isEqualTo(false)
+    }
+}
---
+++ Test/src/test/kotlin/ProjectFile.kt
@@ -0,0 +1,9 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+class ProjectFile {
+    @Test
+    fun test() {
+        assertThat(true).isEqualTo(true)
+        assertThat(false).isEqualTo(false)
+    }
+}
